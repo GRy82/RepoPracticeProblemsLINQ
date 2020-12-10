db34e99ad9b4d50916e99326ca1ee8fbaaa5e47f
GRy82 <gwryan@buffalo.edu>
2020-12-10 13:28:48 -0500

    problems 1 and 2! written, tested, and true!

--- PracticeProblemsLINQ/Program.cs
+++ PracticeProblemsLINQ/Program.cs
@@ -12,41 +12,53 @@ namespace PracticeProblemsLINQ
         {
             //Problem 1
             List<string> words = new List<string>() { "the", "bike", "this", "it", "tenth", "mathematics" };
-            LinqProblems.RunProblem1(words);
+            words = LinqProblems.RunProblem1(words);
+            foreach(string word in words)
+            {
+                Console.WriteLine(word);
+            }
+            Console.WriteLine();
+
 
             //Problem 2
             List<string> names = new List<string>() { "Mike", "Brad", "Nevin", "Ian", "Mike" };
-            LinqProblems.RunProblem2(names);
-
-
-            //Problem 3 & Problem 4
-            List<Customer> customers = new List<Customer>()
+            names = LinqProblems.RunProblem2(names);
+            foreach (string name in names)
             {
-                new Customer(1, "Mike", "Rodgers"),
-                new Customer(2, "Nick", "Allen"),
-                new Customer(3, "Jason", "Ryan"),
-                new Customer(4, "Dan", "Laffey")
-            };
-            LinqProblems.RunProblem3(customers);
-            LinqProblems.RunProblem4(customers);
+                Console.WriteLine(name);
+            }
+            Console.ReadLine();
+            Console.WriteLine();
 
 
-            //Problem 5
-            List<string> classGrades = new List<string>()
-            {
-                "80,100,92,89,65", 
-                "93,81,78,84,69",
-                "73,88,83,99,64",
-                "98,100,66,74,55"
-            };
+            //    //Problem 3 & Problem 4
+            //    List<Customer> customers = new List<Customer>()
+            //    {
+            //        new Customer(1, "Mike", "Rodgers"),
+            //        new Customer(2, "Nick", "Allen"),
+            //        new Customer(3, "Jason", "Ryan"),
+            //        new Customer(4, "Dan", "Laffey")
+            //    };
+            //    LinqProblems.RunProblem3(customers);
+            //    LinqProblems.RunProblem4(customers);
 
-            LinqProblems.RunProblem5(classGrades);
+
+            //    //Problem 5
+            //    List<string> classGrades = new List<string>()
+            //    {
+            //        "80,100,92,89,65", 
+            //        "93,81,78,84,69",
+            //        "73,88,83,99,64",
+            //        "98,100,66,74,55"
+            //    };
+
+            //    LinqProblems.RunProblem5(classGrades);
 
 
-            //--------------------
-            //Bonus Problem 1
-            string letters = "terrill";
-            LinqProblems.RunBonusProblem1(letters);
+            //    //--------------------
+            //    //Bonus Problem 1
+            //    string letters = "terrill";
+            //    LinqProblems.RunBonusProblem1(letters);
         }
     }
 }
